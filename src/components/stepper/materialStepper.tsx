a1cb76ab1aecdaf77fc849395bdcda7c7665fc10
Trestx <info@trestx.com>
2022-01-03 18:57:34 +0530

    Changes

--- src/components/stepper/materialStepper.tsx
+++ src/components/stepper/materialStepper.tsx
@@ -14,10 +14,10 @@ import { Circle } from '@mui/icons-material';
 import { StepIconProps } from '@mui/material/StepIcon';
 import { styled } from '@mui/material/styles';
 import BasicProposalForm from '../forms/proposal/basic_proposal_form';
-import { CustomizedPButtonsNI, CustomizedPrimeButton} from '../buttons/pbutton';
+import { CustomizedPButtonsNI, CustomizedPrimeButton } from '../buttons/pbutton';
 import ProposalBuilderForm from '../forms/proposal/proposal_builder_form';
 import { Dialog } from 'primereact/dialog';
-import {ReqDiscount} from '../dialog/primedialog';
+import { ReqDiscount } from '../dialog/primedialog';
 import { useState } from 'react';
 const steps = [
     {
@@ -91,19 +91,20 @@ function ColorlibStepIcon(props: StepIconProps) {
     );
 }
 
-export default function MaterialStepper({manageService,setManageService,dataPro,setDataPro, network,setNetwork, setCampaignType,
-    campaignObj, setCampaignObj, audience,setAudience,
+export default function MaterialStepper({ manageService, setManageService, dataPro, setDataPro, network, setNetwork, setCampaignType,
+    campaignObj, setCampaignObj, audience, setAudience,
     channel, setChannel, bidStr, setBidstr, durationf,
     durationt, reach, businessRule, freq, setDurationf, setDurationt,
-    setReach, setBusinessRule,setFreq, agency,client,brand,businessObj,market,setAgency,setClient,setBrand,setBusinessObj,setMarket}:{
-        manageService:boolean,setManageService:React.Dispatch<React.SetStateAction<boolean>>,dataPro:string,setDataPro:React.Dispatch<React.SetStateAction<string>>,
-        network:string,setNetwork:React.Dispatch<React.SetStateAction<string>>, setCampaignType:React.Dispatch<React.SetStateAction<string>>,
-        campaignObj:string, setCampaignObj:React.Dispatch<React.SetStateAction<string>>, audience:string,setAudience:React.Dispatch<React.SetStateAction<string>>,
-        channel:any, setChannel:React.Dispatch<any>, bidStr:any, setBidstr: React.Dispatch<any>, durationf:any,
-        durationt:any, reach:string, businessRule:boolean, freq:any, setDurationf:React.Dispatch<any>, setDurationt:React.Dispatch<any>,
-        setReach:React.Dispatch<React.SetStateAction<string>>, setBusinessRule:React.Dispatch<React.SetStateAction<boolean>>,setFreq:React.Dispatch<any>, agency:string,client:string,brand:string,businessObj:string,market:any,setAgency:React.Dispatch<React.SetStateAction<string>>,setClient:React.Dispatch<React.SetStateAction<string>>,setBrand:React.Dispatch<React.SetStateAction<string>>,setBusinessObj:React.Dispatch<React.SetStateAction<string>>,setMarket:any }) {
+    setReach, setBusinessRule, setFreq, agency, client, brand, businessObj, market, setAgency, setClient, setBrand, setBusinessObj, setMarket }: {
+        manageService: boolean, setManageService: React.Dispatch<React.SetStateAction<boolean>>, dataPro: string, setDataPro: React.Dispatch<React.SetStateAction<string>>,
+        network: string, setNetwork: React.Dispatch<React.SetStateAction<string>>, setCampaignType: React.Dispatch<React.SetStateAction<string>>,
+        campaignObj: string, setCampaignObj: React.Dispatch<React.SetStateAction<string>>, audience: string, setAudience: React.Dispatch<React.SetStateAction<string>>,
+        channel: any, setChannel: React.Dispatch<any>, bidStr: any, setBidstr: React.Dispatch<any>, durationf: any,
+        durationt: any, reach: string, businessRule: boolean, freq: any, setDurationf: React.Dispatch<any>, setDurationt: React.Dispatch<any>,
+        setReach: React.Dispatch<React.SetStateAction<string>>, setBusinessRule: React.Dispatch<React.SetStateAction<boolean>>, setFreq: React.Dispatch<any>, agency: string, client: string, brand: string, businessObj: string, market: any, setAgency: React.Dispatch<React.SetStateAction<string>>, setClient: React.Dispatch<React.SetStateAction<string>>, setBrand: React.Dispatch<React.SetStateAction<string>>, setBusinessObj: React.Dispatch<React.SetStateAction<string>>, setMarket: any
+    }) {
     const [activeStep, setActiveStep] = React.useState(0);
-    const [openDialog,setOpenDialog] = useState(false);
+    const [openDialog, setOpenDialog] = useState(false);
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
     };
@@ -120,36 +121,44 @@ export default function MaterialStepper({manageService,setManageService,dataPro,
 
     return (
         <>
-        <Box sx={{ maxWidth: 750 }}>
-            <Stepper activeStep={activeStep} orientation="vertical">
-                {steps.map((step, index) => (
-                    <Step key={step.label} >
-                        <StepLabel StepIconComponent={ColorlibStepIcon} onClick={() => { activeStep === 0 ? handleNext() : handleBack(); }}>
-                            <div style={{ padding: '10px', background: 'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)', borderRadius: '7px 7px 0px 0px', textAlign: 'left', color: 'white', fontWeight: 600, fontSize: '12px', }}>
-                                <span style={{ marginLeft: '10px' }}>{step.label}</span>
-                            </div>
-                        </StepLabel>
-                        <StepContent>
-                            {index === 0 ? <BasicProposalForm agency={agency} client={client} brand={brand} businessObj={businessObj} market={market} setAgency={setAgency} setClient={setClient} setBrand={setBrand} setBusinessObj={setBusinessObj} setMarket={setMarket} onNext={handleNext}/> : <ProposalBuilderForm manageService={manageService} setManageService={setManageService}
-     dataPro={dataPro} setDataPro={setDataPro} network={network} setNetwork={setNetwork} setCampaignType={setCampaignType}
-campaignObj={campaignObj} setCampaignObj={setCampaignObj} audience={audience} setAudience={setAudience}
-channel={channel} setChannel={setChannel} bidStr={bidStr} setBidstr={setBidstr} durationf={durationf}
-durationt={durationt} reach={reach} businessRule={businessRule} freq={freq} setDurationf={setDurationf} setDurationt={setDurationt}
-setReach={setReach} setBusinessRule={setBusinessRule} setFreq={setFreq}/>}
-                        </StepContent>
-                    </Step>
-                ))}
-            </Stepper>
-            <h1 style={{fontSize:'12px',marginTop:'15px',textAlign:'left',color:'#6b42f6',marginLeft:'40px',cursor:'pointer'}} onClick={()=>{setOpenDialog(true);}}>Raise a request for discount</h1>
-            <span style={{float:'right'}}><CustomizedPrimeButton label={'Save'} fw={600} background={'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)'} brad={'25px'} color={'white'}fs={'12px'} onClickHandler={()=>{console.log('empty');}}/>
-            <CustomizedPrimeButton label={'Preview'} fw={600} background={'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)'} brad={'25px'} color={'white'}fs={'12px'} onClickHandler={()=>{console.log('empty');}}/>
-            <CustomizedPrimeButton label={'Submit'} fw={600} background={'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)'} brad={'25px'} color={'white'}fs={'12px'} onClickHandler={()=>{console.log('empty');}}/>
-            </span>
-        </Box>
+            <Box sx={{ maxWidth: 750 }}>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                    {steps.map((step, index) => (
+                        <Step key={step.label} >
+                            <StepLabel StepIconComponent={ColorlibStepIcon} onClick={() => { activeStep === 0 ? handleNext() : handleBack(); }}>
+                                <div style={{ padding: '10px', background: 'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)', borderRadius: '7px 7px 0px 0px', textAlign: 'left', color: 'white', fontWeight: 600, fontSize: '12px', }}>
+                                    <span style={{ marginLeft: '10px' }}>{step.label}</span>
+                                </div>
+                            </StepLabel>
+                            <StepContent>
+                                {index === 0 ? <BasicProposalForm agency={agency} client={client} brand={brand} businessObj={businessObj} market={market} setAgency={setAgency} setClient={setClient} setBrand={setBrand} setBusinessObj={setBusinessObj} setMarket={setMarket} onNext={handleNext} /> : <ProposalBuilderForm manageService={manageService} setManageService={setManageService}
+                                    dataPro={dataPro} setDataPro={setDataPro} network={network} setNetwork={setNetwork} setCampaignType={setCampaignType}
+                                    campaignObj={campaignObj} setCampaignObj={setCampaignObj} audience={audience} setAudience={setAudience}
+                                    channel={channel} setChannel={setChannel} bidStr={bidStr} setBidstr={setBidstr} durationf={durationf}
+                                    durationt={durationt} reach={reach} businessRule={businessRule} freq={freq} setDurationf={setDurationf} setDurationt={setDurationt}
+                                    setReach={setReach} setBusinessRule={setBusinessRule} setFreq={setFreq} />}
+                            </StepContent>
+                        </Step>
+                    ))}
+                </Stepper>
+                {activeStep === 1 &&
+                    <div className='p-grid'>
+                        <div className='p-col-7'>
+                            <h1 style={{ fontSize: '12px', marginTop: '15px', textAlign: 'left', color: '#6b42f6', marginLeft: '40px', cursor: 'pointer' }} onClick={() => { setOpenDialog(true); }}>Raise a request for discount</h1>
 
-        <Dialog header="" visible={openDialog} style={{ width: '40vw',borderRadius:'19px' }} onHide={() => {setOpenDialog(false);}}>
-            <ReqDiscount/>
-        </Dialog>
+                        </div>
+                        <div className='p-col-5' style={{ marginTop: '15px' }}>
+                            <CustomizedPrimeButton label={'Save'} fw={600} background={'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)'} brad={'25px'} color={'white'} fs={'12px'} onClickHandler={() => { console.log('empty'); }} />
+                            <CustomizedPrimeButton label={'Preview'} fw={600} background={'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)'} brad={'25px'} color={'white'} fs={'12px'} onClickHandler={() => { console.log('empty'); }} />
+                            <CustomizedPrimeButton label={'Submit'} fw={600} background={'linear-gradient(to right,#A45BE4 0%, #6B42F6 70%)'} brad={'25px'} color={'white'} fs={'12px'} onClickHandler={() => { console.log('empty'); }} />
+
+                        </div>
+                    </div>}
+            </Box>
+
+            <Dialog header="" visible={openDialog} style={{ width: '40vw', borderRadius: '19px' }} onHide={() => { setOpenDialog(false); }}>
+                <ReqDiscount />
+            </Dialog>
         </>
     );
 }
--- src/components/tables/managetable.tsx
+++ src/components/tables/managetable.tsx
@@ -3,115 +3,115 @@ import { Column } from 'primereact/column';
 import React, { useState, useEffect, useRef } from 'react';
 import { Menu } from 'primereact/menu';
 import './managetable.css';
-import {data,ellipiseitems,ellipiseitemsToDo} from './managetableconst';
+import { data, ellipiseitems, ellipiseitemsToDo } from './managetableconst';
 import { Dialog } from 'primereact/dialog';
-import {ManageProposalActivationDialogCon, ManageProposalDialogCon, ManageProposalRejectDialogCon} from '../dialog/primedialog';
+import { ManageProposalActivationDialogCon, ManageProposalDialogCon, ManageProposalRejectDialogCon } from '../dialog/primedialog';
 import { brandBodyTemplate, campaignsBodyTemplate, clientNameBodyTemplate, createdBodyTemplate, dataProductBodyTemplate, durationBodyTemplate, estimateBudgetBodyTemplate, proposalNumbBodyTemplate, proposalNumbVBodyTemplate, saleRepBodyTemplate } from './commonmangaetablestyle';
 import { Toast } from 'primereact/toast';
 const ManageColTable = () => {
     const toast = useRef(null);
-    const [dateRange,setDateRange] = useState<Date | Date[] | undefined>(undefined);
+    const [dateRange, setDateRange] = useState<Date | Date[] | undefined>(undefined);
     const [expandedRows, setExpandedRows] = useState(null);
-    const [approvedD,setApprovedD] = useState(false);
-    const [rejectedD,setRejectedD] = useState(false);
-    const [submitD,setSubmitD] = useState(false);
+    const [approvedD, setApprovedD] = useState(false);
+    const [rejectedD, setRejectedD] = useState(false);
+    const [submitD, setSubmitD] = useState(false);
     const menu = useRef(null);
     const menu1 = useRef(null);
     const items = [
         {
             label: 'Approved',
             icon: 'pi pi-check',
-            command:()=>{
+            command: () => {
                 setApprovedD(true);
             }
         },
         {
             label: 'Reply awaited',
             icon: 'pi pi-pause',
-            command:()=>{
+            command: () => {
                 setSubmitD(true);
             }
         },
         {
             label: 'Rejected',
             icon: 'pi pi-exclamation-circle',
-            command:()=>{
+            command: () => {
                 setRejectedD(true);
             }
         },
     ];
 
 
-   const proposalStatBodyTemplate = (rowData) => {
-    
+    const proposalStatBodyTemplate = (rowData) => {
+
         return <>
-        <Menu model={items} popup ref={menu1} id="popup_menu" style={{fontSize:'10px',color:'gray',borderRadius:'14px',maxWidth:'160px'}}/>
-        <span className={`${rowData && rowData.proposalStatus && rowData.proposalStatus.replace(/ /g, '').toLowerCase()}`} style={{fontSize:'11px',textAlign:'center'}}>
-            {rowData.proposalStatus} {sessionStorage.getItem('accountType') && sessionStorage.getItem('accountType')==='admin' && (rowData.proposalStatus==='Submitted' || rowData.proposalStatus==='In Review') && <i className="pi pi-chevron-down" style={{fontSize:'11px',marginLeft:'6px',marginTop:'5px',cursor:'pointer'}} onClick={(event) => menu1.current.toggle(event)} aria-controls="popup_menu" aria-haspopup ></i>}</span></>;
+            <Menu model={items} popup ref={menu1} id="popup_menu" style={{ fontSize: '10px', color: 'gray', borderRadius: '14px', maxWidth: '160px' }} />
+            <span className={`${rowData && rowData.proposalStatus && rowData.proposalStatus.replace(/ /g, '').toLowerCase()}`} style={{ fontSize: '11px', textAlign: 'center' }}>
+                {rowData.proposalStatus} {sessionStorage.getItem('accountType') && sessionStorage.getItem('accountType') !== 'admin' && (rowData.proposalStatus === 'Submitted' || rowData.proposalStatus === 'In Review') && <i className="pi pi-chevron-down" style={{ fontSize: '11px', marginLeft: '6px', marginTop: '5px', cursor: 'pointer' }} onClick={(event) => menu1.current.toggle(event)} aria-controls="popup_menu" aria-haspopup ></i>}</span></>;
     };
-    
+
     const rowExpansionTemplate = (daa) => {
         return (
-            <div className="p-card" style={{borderRadius:'18px'}}>
+            <div className="p-card" style={{ borderRadius: '18px' }}>
                 <DataTable value={data} responsiveLayout="scroll">
-                    <Column field="proposalNumber" header="Proposal Version"  body={proposalNumbVBodyTemplate}/>
-                    <Column field="clientName" header="Client Name" body={clientNameBodyTemplate}/>
-                    <Column field="brand" header="Brand" body={brandBodyTemplate}/>
-                    <Column field="duration" header="Duration"  body={durationBodyTemplate}/>
-                    <Column field="estimatedBudget" header="Estimated Budget" body={estimateBudgetBodyTemplate} sortable/>
-                    <Column field="dataProduct" header="DataProduct" body={dataProductBodyTemplate}/>
-                    <Column field="proposalStatus" header="Proposal Status" body={proposalStatBodyTemplate}/>
-                    <Column field="campaigns" header="Campaigns"  body={campaignsBodyTemplate} sortable/>
-                    <Column field="created" header="Created" body={createdBodyTemplate}/>
-                    {sessionStorage.getItem('accountType') && sessionStorage.getItem('accountType')==='admin' && <Column field="saleRep" header="Sales Rep" body={saleRepBodyTemplate}/>}
-                    <Column style={{ width: '1em'}} body={actionBodyTemplate}/>
+                    <Column field="proposalNumber" header="Proposal Version" body={proposalNumbVBodyTemplate} />
+                    <Column field="clientName" header="Client Name" body={clientNameBodyTemplate} />
+                    <Column field="brand" header="Brand" body={brandBodyTemplate} />
+                    <Column field="duration" header="Duration" body={durationBodyTemplate} />
+                    <Column field="estimatedBudget" header="Estimated Budget" body={estimateBudgetBodyTemplate} sortable />
+                    <Column field="dataProduct" header="DataProduct" body={dataProductBodyTemplate} />
+                    <Column field="proposalStatus" header="Proposal Status" body={proposalStatBodyTemplate} />
+                    <Column field="campaigns" header="Campaigns" body={campaignsBodyTemplate} sortable />
+                    <Column field="created" header="Created" body={createdBodyTemplate} />
+                    {sessionStorage.getItem('accountType') && sessionStorage.getItem('accountType') === 'admin' && <Column field="saleRep" header="Sales Rep" body={saleRepBodyTemplate} />}
+                    <Column style={{ width: '1em' }} body={actionBodyTemplate} />
                 </DataTable>
             </div>
         );
     };
-    const actionBodyTemplate = (rowData)=>{
-        if (rowData.proposalStatus==='To Do'){
-            return <><Menu model={ellipiseitemsToDo} popup ref={menu} id="popup_menu" style={{fontSize:'10px',color:'gray',borderRadius:'14px',maxWidth:'150px'}}/><i className="pi pi-ellipsis-h" onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup ></i></>;
-        }else{
-            return <><Menu model={ellipiseitems} popup ref={menu} id="popup_menu" style={{fontSize:'10px',color:'gray',borderRadius:'14px',maxWidth:'150px'}}/><i className="pi pi-ellipsis-h" onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup ></i></>;
+    const actionBodyTemplate = (rowData) => {
+        if (rowData.proposalStatus === 'To Do') {
+            return <><Menu model={ellipiseitemsToDo} popup ref={menu} id="popup_menu" style={{ fontSize: '10px', color: 'gray', borderRadius: '14px', maxWidth: '150px' }} /><i className="pi pi-ellipsis-h" onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup ></i></>;
+        } else {
+            return <><Menu model={ellipiseitems} popup ref={menu} id="popup_menu" style={{ fontSize: '10px', color: 'gray', borderRadius: '14px', maxWidth: '150px' }} /><i className="pi pi-ellipsis-h" onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup ></i></>;
         }
-        
+
     };
 
     return (
         <>
-        <Toast ref={toast} />
-        <div >
-            <div className="p-shadow-5" >
-                <DataTable value={data} expandedRows={expandedRows} onRowToggle={(e) => setExpandedRows(e.data)}
-                    responsiveLayout="scroll"
-                    rowExpansionTemplate={rowExpansionTemplate} dataKey="id" stripedRows paginator rows={5}>
+            <Toast ref={toast} />
+            <div >
+                <div className="p-shadow-5" >
+                    <DataTable value={data} expandedRows={expandedRows} onRowToggle={(e) => setExpandedRows(e.data)}
+                        responsiveLayout="scroll"
+                        rowExpansionTemplate={rowExpansionTemplate} dataKey="id" stripedRows paginator rows={5}>
 
-                    <Column field="proposalNumber" header="Proposal number"  body={proposalNumbBodyTemplate}/>
-                    <Column field="clientName" header="Client Name" body={clientNameBodyTemplate}/>
-                    <Column field="brand" header="Brand" body={brandBodyTemplate}/>
-                    <Column field="duration" header="Duration"  body={durationBodyTemplate}/>
-                    <Column field="estimatedBudget" header="Estimated Budget" body={estimateBudgetBodyTemplate} sortable/>
-                    <Column field="dataProduct" header="DataProduct" body={dataProductBodyTemplate}/>
-                    <Column field="proposalStatus" header="Proposal Status" body={proposalStatBodyTemplate}/>
-                    <Column field="campaigns" header="Campaigns"  body={campaignsBodyTemplate} sortable/>
-                    <Column field="created" header="Created" body={createdBodyTemplate}/>
-                    {sessionStorage.getItem('accountType') && sessionStorage.getItem('accountType')==='admin' && <Column field="saleRep" header="Sales Rep" body={saleRepBodyTemplate}/>}
-                    <Column expander style={{ width: '1em'}}/>
-                    <Column style={{ width: '1em'}} body={actionBodyTemplate}/>
-                </DataTable>
+                        <Column field="proposalNumber" header="Proposal number" body={proposalNumbBodyTemplate} />
+                        <Column field="clientName" header="Client Name" body={clientNameBodyTemplate} />
+                        <Column field="brand" header="Brand" body={brandBodyTemplate} />
+                        <Column field="duration" header="Duration" body={durationBodyTemplate} />
+                        <Column field="estimatedBudget" header="Estimated Budget" body={estimateBudgetBodyTemplate} sortable />
+                        <Column field="dataProduct" header="DataProduct" body={dataProductBodyTemplate} />
+                        <Column field="proposalStatus" header="Proposal Status" body={proposalStatBodyTemplate} />
+                        <Column field="campaigns" header="Campaigns" body={campaignsBodyTemplate} sortable />
+                        <Column field="created" header="Created" body={createdBodyTemplate} />
+                        {sessionStorage.getItem('accountType') && sessionStorage.getItem('accountType') === 'admin' && <Column field="saleRep" header="Sales Rep" body={saleRepBodyTemplate} />}
+                        <Column expander style={{ width: '1em' }} />
+                        <Column style={{ width: '1em' }} body={actionBodyTemplate} />
+                    </DataTable>
+                </div>
             </div>
-        </div>
-        <Dialog header="" visible={approvedD} style={{ width: '40vw',borderRadius:'19px' }} onHide={() => setApprovedD(false)}>
-            <ManageProposalDialogCon dateR={dateRange} setDate={setDateRange}/>
-        </Dialog>
-        <Dialog header="" visible={rejectedD} style={{ width: '30vw',borderRadius:'19px' }} onHide={() => setRejectedD(false)}>
-            <ManageProposalRejectDialogCon toas={toast}/>
-        </Dialog>
-        <Dialog header="" visible={submitD} style={{ width: '45vw',borderRadius:'19px' }} onHide={() => setSubmitD(false)}>
-        <ManageProposalActivationDialogCon/>
-        </Dialog>
-        
+            <Dialog header="" visible={approvedD} style={{ width: '40vw', borderRadius: '19px' }} onHide={() => setApprovedD(false)}>
+                <ManageProposalDialogCon dateR={dateRange} setDate={setDateRange} />
+            </Dialog>
+            <Dialog header="" visible={rejectedD} style={{ width: '30vw', borderRadius: '19px' }} onHide={() => setRejectedD(false)}>
+                <ManageProposalRejectDialogCon toas={toast} />
+            </Dialog>
+            <Dialog header="" visible={submitD} style={{ width: '45vw', borderRadius: '19px' }} onHide={() => setSubmitD(false)}>
+                <ManageProposalActivationDialogCon />
+            </Dialog>
+
         </>
     );
 };
